997733634d62dc52d616136683af1013f9cc774c
elijahchen <code.elijahchen@gmail.com>
2017-03-09 17:15:01 -0500

    - Included body-parser package and added a post route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
+var bodyParser = require("body-parser")
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function (req,res) {
@@ -17,6 +19,10 @@ app.get("/locations", function (req, res) {
         {name: "loc 6", image: "http://i.imgur.com/K5maUDc.jpg"},
     ];
     res.render("locations", {locations: locations});
+});
+
+app.post("/locations", function (req, res) {
+    res.send("POST ROUTE HERE");
 });
 
 app.listen(3000, process.env.IP, function () {
