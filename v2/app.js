d2a0f5dd217b3996da14c92d55cc0438e82945c0
elijahchen <code.elijahchen@gmail.com>
2017-03-13 10:03:33 -0400

    - Included mongoose dependency

--- v2/app.js
+++ v2/app.js
@@ -1,6 +1,7 @@
-var express = require("express");
-var bodyParser = require("body-parser")
-var app = express();
+var express     = require("express"),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    app         = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
