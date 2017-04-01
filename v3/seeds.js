ebce230b151fad1b2a7a0aa229ccff0822a81aad
WINDOWS-IFPK7JI\Elijah <code.elijahchen@gmail.com>
2017-04-01 14:03:53 -0400

    - Populated seed.js with data

--- v3/seeds.js
+++ v3/seeds.js
@@ -1,13 +1,38 @@
 const mongoose = require("mongoose");
-const Campground = require("./models/mLocation");
+const Location = require("./models/mLocation");
+
+let data = [
+    {
+        name: "Chelsea Landmark Leasing",
+        image: "https://mgnsw.org.au/media/thumbs/uploads/images/DSC_0062_2.jpg.600x400_q85_crop_upscale.jpg",
+        description: "This corner 1BR/1BA has views to the south and east, floor-to-ceiling windows in the living room, and a separate windowed kitchen. The apartment also includes a washer and dryer and walk-in closet. Schedule a private tour today."
+    },{
+        name: "Heath Morgan Habitats",
+        image: "http://playtivities.com/wp-content/uploads/2015/01/simple-cardboard-house-600x399.jpg",
+        description: "BUILDING AMENITIES Laundry Room Gas Included Electric Included Live in Super Pets Allowed Heat Included Video Intercom Hot Water Included "
+    },{
+        name: "Gewurtz Properties",
+        image: "https://c.fastcompany.net/multisite_files/fastcompany/imagecache/slideshow_large/slideshow/2013/11/3021937-slide-750-origami-02.jpg",
+        description: "NO FEE. Beautiful open bedroom available for rent in amazing location in East Greenwich Village."
+    }
+];
 
 function seedDB() {
     // Remove all listings
-    Campground.remove({}, function (err) {
+    Location.remove({}, function (err) {
         if(err){
             console.log(err);
         }
         console.log("Removed location!")
+    });
+    data.forEach(function (seed) {
+       Location.create(seed, function (err, data) {
+          if(err){
+              console.log(err);
+          } else {
+              console.log("Added a new location!");
+          }
+       });
     });
 }
 
